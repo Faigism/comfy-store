c8ab117aa71d0acaaf4bee0395e86b55277f6d4b
Faig Ismayilov <faigyi@code.edu.az>
2024-06-30 15:42:07 +0400

    comfy-store sites pages added

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  HomeLayout,
+  Landing,
+  Login,
+  Orders,
+  Products,
+  Register,
+  SingleProduct,
+} from './pages'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
+    </div>
+  )
+}
+export default App
